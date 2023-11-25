cc8358bbdb9780327a5dde69da139c9d95e095b7
angelomar <angelomarcia98@gmail.com>
2023-11-25 01:30:04 +0100

    git

--- src/ts/controller.ts
+++ src/ts/controller.ts
@@ -1,47 +1,43 @@
-/*
-USER STORIES:
-- Come utente voglio poter ricercare qualsiasi pokèmon con i 4 filtri, più fully evolved toggle;
-- Come utente voglio vedere disposti tutti i pokèmon in una griglia;
-- Voglio che al click su un pokèmon nella griglia di ricerca, venga inserito nel mio team;
-- Voglio poter vedere il mio team, ogni pokèmon deve avere un nome, uno sprite e i tipi;
-- Con un menu a sidebar o semplicemente nella pagina per desktop voglio poter scegliere mosse e abilità tra quelle disponibili;
-- Completato il team voglio poter vedere le sue statistiche in termini di coverage e difesa;
-- Voglio poter assegnare un nome al team;
+// /*
+// USER STORIES:
+// - Come utente voglio poter ricercare qualsiasi pokèmon con i 4 filtri, più fully evolved toggle;
+// - Come utente voglio vedere disposti tutti i pokèmon in una griglia;
+// - Voglio che al click su un pokèmon nella griglia di ricerca, venga inserito nel mio team;
+// - Voglio poter vedere il mio team, ogni pokèmon deve avere un nome, uno sprite e i tipi;
+// - Con un menu a sidebar o semplicemente nella pagina per desktop voglio poter scegliere mosse e abilità tra quelle disponibili;
+// - Completato il team voglio poter vedere le sue statistiche in termini di coverage e difesa;
+// - Voglio poter assegnare un nome al team;
 
-- Voglio che il team sia salvato con tutte le sue info nella side bar in mobile e attraverso un menù con bottone su desktop;
-- Al click del team voglio sia disposto per intero nello suo slot nella pagina, magari con menu per le mosse aperte on mobile;
+// - Voglio che il team sia salvato con tutte le sue info nella side bar in mobile e attraverso un menù con bottone su desktop;
+// - Al click del team voglio sia disposto per intero nello suo slot nella pagina, magari con menu per le mosse aperte on mobile;
 
+// FEATURES:
+// - Tutto parte dal load della pagina, controlla il local storage e carica i team salvati nei preferiti;
+// - Nessun pokèmon è disposto all'inizio, solo dopo una ricerca operiamo per la griglia;
+// - La funzione di ricerca usa 3 filtri, più un toggle per il fully evolved. L'ordine di operazione va in questo modo: Prendi tutti quelli di una gen, carica TUTTE le forms (ci sono sempre), e carica TUTTE le variants che rispettano la gen.  Quindi è, per ogni pokèmon: GEN E FULLY EVO CHECK, CARICA OGNI FORM DOPO, CARICA OGNI VARIANTE CHE RISPETTA LA GEN. Poi FILTRA PER TIPO (questo è perché alcune varianti potrebbero essere filtrate con questo metodo), e si opera su questa lista per ricerca di nome, lettera per lettera;
+// - Questa lista ci darà semplicemente una lista di ID, ID che usiamo per le immagini. Disponiamo in un una griglia tutti le entità che hanno passato il filtro;
+// - Ogni immagine modifica l'URL col suo ID unico;
+// - Alla modifica dell'url aggiungiamo il pokèmon nel team;
+// - Nello slot del team il pokèmon viene disposto con sprite, nome, e tipo, più un menù toggle per mosse e abilità. Per due eccezioni, quali Silvally ed Arceus si può scegliere il tipo, la loro mossa peculiare rispecchia il tipo;
+// - C'è un bottone per poter salvare il team e dargli un nome;
+// - C'è un side menu che dispone i team salvati con icone da menu (se ce ne sono) E le statistiche del team corrente(coverage e difesa);
+// - Al click di qualsiasi team salvato, viene disposto nello slot team nella pagina;
 
-FEATURES:
-- Tutto parte dal load della pagina, controlla il local storage e carica i team salvati nei preferiti;
-- Nessun pokèmon è disposto all'inizio, solo dopo una ricerca operiamo per la griglia;
-- La funzione di ricerca usa 3 filtri, più un toggle per il fully evolved. L'ordine di operazione va in questo modo: Prendi tutti quelli di una gen, carica TUTTE le forms (ci sono sempre), e carica TUTTE le variants che rispettano la gen.  Quindi è, per ogni pokèmon: GEN E FULLY EVO CHECK, CARICA OGNI FORM DOPO, CARICA OGNI VARIANTE CHE RISPETTA LA GEN. Poi FILTRA PER TIPO (questo è perché alcune varianti potrebbero essere filtrate con questo metodo), e si opera su questa lista per ricerca di nome, lettera per lettera;
-- Questa lista ci darà semplicemente una lista di ID, ID che usiamo per le immagini. Disponiamo in un una griglia tutti le entità che hanno passato il filtro;
-- Ogni immagine modifica l'URL col suo ID unico;
-- Alla modifica dell'url aggiungiamo il pokèmon nel team;
-- Nello slot del team il pokèmon viene disposto con sprite, nome, e tipo, più un menù toggle per mosse e abilità. Per due eccezioni, quali Silvally ed Arceus si può scegliere il tipo, la loro mossa peculiare rispecchia il tipo;
-- C'è un bottone per poter salvare il team e dargli un nome;
-- C'è un side menu che dispone i team salvati con icone da menu (se ce ne sono) E le statistiche del team corrente(coverage e difesa);
-- Al click di qualsiasi team salvato, viene disposto nello slot team nella pagina;
+// QUERIES:
+// 1-1017 forme basi
+// 10_001-10_263 forme extra
+// - Get every Pokèmon: https://pokeapi.co/api/v2/pokemon?limit=15000;
+// - Get single pokèmon: https://pokeapi.co/api/v2/pokemon/{id or name}/
+// - Get pokèmon move: https://pokeapi.co/api/v2/move/9/
+// - Get pokèmon ability: https://pokeapi.co/api/v2/ability/{id or name}/
+// - Get evolution chain, check if it's fully evolved or not: https://pokeapi.co/api/v2/evolution-chain/3/
+//   The value "evolves_to" needs to be empty
 
-QUERIES:
-1-1017 forme basi
-10_001-10_263 forme extra
-- Get every Pokèmon: https://pokeapi.co/api/v2/pokemon?limit=15000;
-- Get single pokèmon: https://pokeapi.co/api/v2/pokemon/{id or name}/
-- Get pokèmon move: https://pokeapi.co/api/v2/move/9/
-- Get pokèmon ability: https://pokeapi.co/api/v2/ability/{id or name}/
-- Get evolution chain, check if it's fully evolved or not: https://pokeapi.co/api/v2/evolution-chain/3/
-  The value "evolves_to" needs to be empty
-
-
-Prendi tipo, gen, name, evoluto o no
-
-*/
+// Prendi tipo, gen, name, evoluto o no
 
 // // FIX WHEN DEPLOYING
-// import "node_modules/core-js/stable";
-// import "regenerator-runtime/runtime";
+import "node_modules/core-js/stable";
+import "regenerator-runtime/runtime";
 
 //
 
