60600073169f3f4a84dc8ce5994a578d9bf62294
angelomar <angelomarcia98@gmail.com>
2023-11-25 15:24:39 +0100

    git

--- src/js/model.js
+++ src/js/model.js
@@ -7,6 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+import "regenerator-runtime";
 import * as pokedex from "./pokèdex/pokedex.js";
 import forms from "./pokèdex/forms.js";
 import megas from "./pokèdex/megas.js";
